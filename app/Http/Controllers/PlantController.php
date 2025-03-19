5d90b7aa459e19b1382a569b9f0f0452a2cf183e
ilyass anida <anidailyass@gmail.com>
2025-03-19 15:31:15 +0000

    feat: (PEAP-19) Develop the create plant endpoint.

--- app/Http/Controllers/PlantController.php
+++ app/Http/Controllers/PlantController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePlantRequest;
+use App\Models\Image;
 use App\Models\Plant;
 use Illuminate\Http\Request;
 
@@ -26,7 +28,7 @@ class PlantController extends Controller
     {
         try {
             $plant = Plant::find($id);
-            
+
             if (!$plant) {
                 return response()->json([
                     "status" => false,
@@ -35,6 +37,31 @@ class PlantController extends Controller
             }
 
             return response()->json($plant, 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                "status" => false,
+                "message" => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function store(StorePlantRequest $request)
+    {
+        try {
+            $plant = Plant::create([
+                "name" => $request->name, 
+                "category_id" => $request->category_id, 
+            ]);
+
+            foreach ($request->images as $url) {
+                Image::create([
+                    "url" => $url,
+                    "plant_id" => $plant->id
+                ]);
+            }
+
+            return response()->json($plant, 201);
 
         } catch (\Throwable $th) {
             return response()->json([
