b6afe56043da4d94d0e2f78efd108ea9c356d47b
ilyass anida <anidailyass@gmail.com>
2025-03-20 10:55:50 +0000

    feat: (PEAP-17) Develop the specefique order endpoint.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -12,6 +13,23 @@ class OrderController extends Controller
             $orders = Order::all();
 
             return response()->json($orders, 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                "status" => false,
+                "message" => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function store()
+    {
+        try {
+            $order = Order::create([
+                "client_id" => Auth::id() ?? 20, 
+            ]);
+
+            return response()->json($order, 201);
 
         } catch (\Throwable $th) {
             return response()->json([
